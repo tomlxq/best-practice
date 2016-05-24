ba14af0486937850b7c29b0ce75d1f809ca9e01a
tomlxq <21429503@qq.com>
2016-05-24 17:44:55 +0800

    add hello world

---
+++ springmvc-helloworld/src/main/java/com/tom/web/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.tom.web.controller; /**
+ * Created by tom on 2016/5/24.
+ */
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        //1、收集参数、验证参数
+        //2、绑定参数到命令对象
+        //3、将命令对象传入业务对象进行业务处理
+        //4、选择下一个页面
+        ModelAndView mv = new ModelAndView();
+        //添加模型数据 可以是任意的POJO对象
+        mv.addObject("message", "Hello World!");
+        mv.addObject("message2", "我是tomLuo!");
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        mv.setViewName("hello");
+        return mv;
+    }
+
+
+}
