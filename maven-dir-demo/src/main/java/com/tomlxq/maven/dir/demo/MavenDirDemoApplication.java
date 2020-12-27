addf6d8b457b4acc42befbb113416051b2f8b3fd
tomlxq <21429503@qq.com>
2020-12-27 09:48:12 +0800

    maven dir introduce

---
+++ maven-dir-demo/src/main/java/com/tomlxq/maven/dir/demo/MavenDirDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tomlxq.maven.dir.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDirDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDirDemoApplication.class, args);
+	}
+
+}
---
+++ maven-dir-demo/src/test/java/com/tomlxq/maven/dir/demo/MavenDirDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomlxq.maven.dir.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDirDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
