6c0bd809c72c3ad53e5b4937986a141504676216
tomluo <21429503@qq.com>
2023-05-14 10:46:33 +0800

    add maven
    add maven

---
+++ gs-spring-boot-maven/src/test/java/com/example/bean/Student.java
@@ -0,0 +1,43 @@
+package com.example.bean;
+
+/**
+ * Created by tom on 2016/12/19.
+ */
+public class Student extends BaseEntity{
+    Integer id;
+    String name;
+    String status;
+    Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ gs-spring-boot-maven/src/test/java/com/example/eeee.java
@@ -0,0 +1,32 @@
+/*
+package com.example;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+*/
+/**
+ * Created by tom on 2016/12/19.
+ *//*
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("your test context.xml")
+public class FooDaoTest {
+    @Autowired
+    private FooDao dao;
+
+    @Transactional
+    public void saveFoo_succeeds() {
+        // test save
+    }
+
+    @Transactional
+    public void saveAndLoadFoo_resultsInSameFieldValues() {
+        // save and load; compare fields from before to after
+    }
+
+    // test custom queries
+}
+*/
